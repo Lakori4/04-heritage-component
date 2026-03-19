920bb64cf100adbb7e87daa13100dce2e5ed2196
Lakori4 <83789283+Lakori4@users.noreply.github.com>
2026-03-19 14:37:09 +0000

    started to fix the css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,19 +15,21 @@ const Page = () => {
 
   const [counter, setCounter] = useState<number>(0)
   const [list, setList] = useState<Bought[]>([])
-  const products = ["Front Wing", "Rear Wing", "PU", "Sidepods"]
+  const products = ["Front Wing", "Rear Wing", "PU", "Floor"]
 
   return (
     <div className="page">
       <div className="comp">
-      <h1>Counters</h1>
-      <Counter count={counter} setCount={setCounter}/>
-      <Counter count={counter} setCount={setCounter}/>
+        <h1>Counters</h1>
+        <Counter count={counter} setCount={setCounter} />
+        <Counter count={counter} setCount={setCounter} />
       </div>
       <div className="comp">
-      <h1>Shopping cart</h1>
-      {products.map((p) => (<Product key={p} productName={p} list={list} setList={setList}/>))}
-      {list.map((e) => (<h2 className="bought" onClick={() => (setList(list.filter( p => p.id !== e.id )))} key={e.id}>{e.name}</h2>))}
+        <h1>Shopping cart</h1>
+        <div className="compShop">
+          <div> <h2>Available Products</h2> {products.map((p) => (<Product key={p} productName={p} list={list} setList={setList} />))}</div>
+          <div> <h2>Products in cart</h2> {list.map((e) => (<h2 className="bought" onClick={() => (setList(list.filter(p => p.id !== e.id)))} key={e.id}>{e.name}</h2>))}</div>
+        </div>
       </div>
 
     </div>
